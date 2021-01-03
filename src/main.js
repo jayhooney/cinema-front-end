ba9af973880df4ed15d769ca4da8c70564189947
jayhooney <hooney731@gmail.com>
2021-01-03 16:02:22 +0900

    fix: cors issue fix
    cors로 인해 서버와 통신이 안되던 이슈 해결

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import Router from "./router";
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER_HOST;
+axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
 createApp(App)
   .use(Router)
